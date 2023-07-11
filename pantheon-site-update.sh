946bd405c50a9c2f3189976b5236ae2f44154566
Ryan Johnson <jryaa7@gmail.com>
2023-07-11 11:43:54 -0600

    added --no-check to update without prompts

--- pantheon-site-update.sh
+++ pantheon-site-update.sh
@@ -31,11 +31,16 @@ terminus_auth_check() {
 
 	# user is logged in so continue
 	else
-		read -p "logged in as $RESPONSE - press [y] to continue or [n] to exit: " login;
-		case $login in
-			[Yy]* ) ;;
-			[Nn]* ) exit 0;;
-		esac
+		# check if the --no-check flag is provided to skip input
+		if [[ "$no_check_flag" == true ]]; then
+			printf "logged into terminus as $RESPONSE \n"
+		else
+			read -p "logged into terminus as $RESPONSE - press [y] to continue or [n] to exit: " login;
+			case $login in
+				[Yy]* ) ;;
+				[Nn]* ) exit 0;;
+			esac
+		fi
 	fi
 }
 
@@ -51,10 +56,10 @@ drupal_check() {
     # check for a drupal site
     if [[ "$FRAMEWORK" =~ $REGEX ]]; then
     	# alls good so continue
-    	echo "valid drupal site so continuing"
+    	printf "valid drupal site so continuing\n"
     # not a drupal site so exit
     else
-		echo "script only works for drupal sites, exiting..."
+		printf "script only works for drupal sites, exiting..."
 		exit 0
     fi
 }
@@ -85,25 +90,31 @@ drupal_prep() {
 	# print out a new line for spacing
 	printf '\n'
 
-	# ask if user wants to backup the live env first
-	read -p "backup live environment? [y/n]  " yn
-	case $yn in
-		[Yy]* ) printf "\ncreating backup of live environment for ${SITENAME}...\n"; 
-				# check for being in git mode
-				CONNECTION=`terminus env:info --field connection_mode -- $SITENAME.dev`
-				if [ "$CONNECTION" != "git" ]; then
-					# set to git mode
-					env_git
-				fi
-				# backup live
-				terminus backup:create ${SITENAME}.live;;
-	esac
+	# check if the --no-check flag is provided to skip input
+	if [[ "$no_check_flag" == true ]]; then
+		printf "skipping backup of live environment\n"
+		read -p "provide a commit to attach to the deployment: " MESSAGEPREP
+	else
+		# ask if user wants to backup the live env first
+		read -p "backup live environment? [y/n]  " yn
+		case $yn in
+			[Yy]* ) printf "\ncreating backup of live environment for ${SITENAME}...\n"; 
+					# check for being in git mode
+					CONNECTION=`terminus env:info --field connection_mode -- $SITENAME.dev`
+					if [ "$CONNECTION" != "git" ]; then
+						# set to git mode
+						env_git
+					fi
+					# backup live
+					terminus backup:create ${SITENAME}.live;;
+		esac
 
-	# check for errors in backing up
-	if [ $? = 1 ]; then
-		$((ERRORS++))
-		echo "[err] error in making backup of live environment"
-		exit 0
+		# check for errors in backing up
+		if [ $? = 1 ]; then
+			$((ERRORS++))
+			echo "[err] error in making backup of live environment"
+			exit 0
+		fi
 	fi
 
 	# check for being in git mode
@@ -134,12 +145,24 @@ drupal_update() {
 		# list the upstream updates first
 		terminus upstream:updates:list --fields=datetime,message,author -- ${SITENAME}.dev
 
-		# has upstream so ask for updates
-		read -p "apply upstream updates? [y/n]  " yn
-		case $yn in
-			[Yy]* ) printf "\napplying upstream updates for ${SITENAME}...\n"; 
-					terminus upstream:updates:apply --updatedb --accept-upstream -- ${SITENAME}.dev
-		esac
+		# check if the --no-check flag is provided to skip input
+		if [[ "$no_check_flag" == true ]]; then
+			printf "\napplying upstream updates for ${SITENAME}...\n"; 
+			terminus upstream:updates:apply --updatedb --accept-upstream -- ${SITENAME}.dev
+
+			# done with updates so let user check
+			printf "\ndev environment updated, applying to test and live"
+		else
+			# has upstream so ask for updates
+			read -p "apply upstream updates? [y/n]  " yn
+			case $yn in
+				[Yy]* ) printf "\napplying upstream updates for ${SITENAME}...\n"; 
+						terminus upstream:updates:apply --updatedb --accept-upstream -- ${SITENAME}.dev
+			esac
+
+			# done with updates so let user check
+			printf "\ndev environment updated, check site if needed - https://dev-${SITENAME}.pantheonsite.io"
+		fi
 	# there is no upstream updates
 	else
 		printf "\nno upstream updates found"
@@ -188,7 +211,7 @@ drupal_update() {
 					# commit changes before pushing
 					read -p "commit changes to dev environment on Pantheon? [y/n] " DEPLOYDEV
 					case $DEPLOYDEV in
-						[Yy]* ) read -p "provide a note to attach to this commit: " MESSAGEDEV
+						[Yy]* ) read -p "provide a commit to attach to this commit: " MESSAGEDEV
 								terminus env:commit --message="$MESSAGEDEV" --force -- ${SITENAME}.dev
 								;;
 						[Nn]* ) exit 0;;
@@ -200,8 +223,7 @@ drupal_update() {
 
 	fi
 
-	# done with updates so let user check
-	printf "\ndev environment updated, check site if needed - https://dev-${SITENAME}.pantheonsite.io \n"
+	
 	
 	# error checking
 	errors_check
@@ -214,28 +236,40 @@ drupal_update() {
 drupal_push() {
 	# print out a new line for spacing and check with user and then move to test
 	printf '\n'
-	read -p "deploy changes to test environment on Pantheon? [y/n] " DEPLOYTEST
-	case $DEPLOYTEST in
-		[Yy]* ) read -p "provide a note to attach to this deployment: " MESSAGE
-				terminus env:deploy --note="$MESSAGE" --updatedb -- ${SITENAME}.test
-				terminus env:clear-cache ${SITENAME}.test
-				;;
-		[Nn]* ) exit 0;;
-	esac
 
-	# done with updates so let user check
-	printf "\ntest environment updated, check site if needed - https://test-${SITENAME}.pantheonsite.io \n"
+	# check if the --no-check flag is provided to skip input
+	if [[ "$no_check_flag" == true ]]; then
+		printf "\napplying to test environment"
+		terminus env:deploy --note="${MESSAGEPREP}" --updatedb -- ${SITENAME}.test
+		terminus env:clear-cache ${SITENAME}.test
 
-	# print out a new line for spacing and ask to deploy to live
-	printf '\n'
-	read -p "deploy changes to live environment on Pantheon? [y/n] " DEPLOYLIVE
-	case $DEPLOYLIVE in
-		[Yy]* ) read -p "provide a note to attach to this deployment: " MESSAGE
-				terminus env:deploy --note="$MESSAGE" --updatedb -- ${SITENAME}.live
-				terminus env:clear-cache ${SITENAME}.live
-				;;
-		[Nn]* ) exit 0;;
-	esac
+		printf "\napplying to live environment"
+		terminus env:deploy --note="${MESSAGEPREP}" --updatedb -- ${SITENAME}.live
+		terminus env:clear-cache ${SITENAME}.live
+	else
+		read -p "deploy changes to test environment on Pantheon? [y/n] " DEPLOYTEST
+		case $DEPLOYTEST in
+			[Yy]* ) read -p "provide a commit to attach to this deployment: " MESSAGE
+					terminus env:deploy --note="$MESSAGE" --updatedb -- ${SITENAME}.test
+					terminus env:clear-cache ${SITENAME}.test
+					;;
+			[Nn]* ) exit 0;;
+		esac
+
+		# done with updates so let user check
+		printf "\ntest environment updated, check site if needed - https://test-${SITENAME}.pantheonsite.io"
+
+		# print out a new line for spacing and ask to deploy to live
+		printf '\n'
+		read -p "deploy changes to live environment on Pantheon? [y/n] " DEPLOYLIVE
+		case $DEPLOYLIVE in
+			[Yy]* ) read -p "provide a commit to attach to this deployment: " MESSAGE
+					terminus env:deploy --note="$MESSAGE" --updatedb -- ${SITENAME}.live
+					terminus env:clear-cache ${SITENAME}.live
+					;;
+			[Nn]* ) exit 0;;
+		esac
+	fi
 }
 
 
@@ -284,13 +318,27 @@ env_sftp() {
 # start the script with an echo message
 printf "\nstarting pantheon site update\n"
 
-# check for logged in user
-terminus_auth_check
+# parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --no-check)
+      no_check_flag=true
+      shift
+      ;;
+    *)
+      argument="$1"
+      shift
+      ;;
+  esac
+done
 
 # check for site name passed
- if [ "$#" -eq  "0" ]; then
- 	# no arguments so get site list and input
- 	# grab the sites and display
+if [[ -n "$argument" ]]; then
+	# set the sitename variable
+	SITENAME=$argument
+else
+	# no arguments so get site list and input
+	# grab the sites and display
 	printf '\nfetching site list...\n'
 	terminus site:list --fields="name,plan_name,framework"
 
@@ -299,10 +347,10 @@ terminus_auth_check
 
 	# set the site and start to update
 	read -p 'enter a site name and press [Enter] to continue: ' SITENAME
-else
-	# set the sitename variable
-	SITENAME=$1
 fi
+
+# check for logged in user
+terminus_auth_check
 
 # check the site for being drupal
 drupal_check
